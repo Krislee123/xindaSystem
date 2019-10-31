9be7ee3a7802f3e17beb865284c21b931f3fd191
Krislee123 <2422072929@qq.com>
2019-10-31 15:48:40 +0800

    xindasystem

---
+++ demo-2/src/main/java/com/datangdu/cn/service/ProductService.java
@@ -0,0 +1,12 @@
+package com.datangdu.cn.service;
+
+import java.util.List;
+
+import com.datangdu.cn.model.product.Product;
+
+
+public interface ProductService {
+	 List<Product> findByName(String name);
+		int insert(Product record);
+		int deleteByPrimaryKey(Integer id);
+}
